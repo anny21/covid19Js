ff17e9e97fc1b46ee0612acd0a92630a2d7312d1
unknown <honyinye3@gmail.com>
2020-04-14 18:31:02 +0100

    commit

--- src/estimator.js
+++ src/estimator.js
@@ -15,16 +15,16 @@ const covid19ImpactEstimator = (data) => {
     periodType,
     timeToElapse,
     reportedCases,
-    region
   } = data;
-    const time = timeInDays(timeToElapse, periodType);
-    estimate.impact.currentlyInfected = reportedCases * 10;
-    estimate.severeImpact.currentlyInfected = reportedCases * 50;
-    const x = estimate.impact.currentlyInfected;
-    const y = estimate.severeImpact.currentlyInfected;
-    estimate.impact.infectionsByRequestedTime = x * 2 ** parseInt(time / 3, 10);
-    const z = y * 2 ** parseInt(time / 3, 10);
-    estimate.severeImpact.infectionsByRequestedTime = z;
+
+  const time = timeInDays(timeToElapse, periodType);
+  estimate.impact.currentlyInfected = reportedCases * 10;
+  estimate.severeImpact.currentlyInfected = reportedCases * 50;
+  const x = estimate.impact.currentlyInfected;
+  const y = estimate.severeImpact.currentlyInfected;
+  estimate.impact.infectionsByRequestedTime = x * 2 ** parseInt(time / 3, 10);
+  const z = y * 2 ** parseInt(time / 3, 10);
+  estimate.severeImpact.infectionsByRequestedTime = z;
 
   return estimate;
 };
