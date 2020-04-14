7b1766ea2fd3f31a216681aec572d56e0ebb34de
unknown <honyinye3@gmail.com>
2020-04-14 16:50:44 +0100

    commit

--- src/estimator.js
+++ src/estimator.js
@@ -2,23 +2,17 @@ const covid19ImpactEstimator = (data) => {
   const output = { data: {}, impact: {}, severeImpact: {} };
   output.impact.currentlyInfected = data.reportedCases * 10;
   output.severeImpact.currentlyInfected = data.reportedCases * 50;
-
   output.severeImpact.infectionsByRequestedTime = output.severeImpact.currentlyInfected * 512;
-  output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * 512;
-  
-   
+  output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * 512; 
   const mpact1 = (15 / 100) * (output.severeImpact.infectionsByRequestedTime);
   output.severeImpact.severeCasesByRequestedTime = mpact1;
   const mpact2 = (15 / 100) * (output.impact.infectionsByRequestedTime);
   output.impact.severeCasesByRequestedTime = mpact2;
-
   const mpact3 = (35 / 100) * (data.totalHospitalBeds) * output.severeImpact.severeCasesByRequestedTime;
   output.severeImpact.hospitalBedsByRequestedTime = mpact3;
-
   const mpact4 = ((35 / 100) * data.totalHospitalBeds) * output.impact.severeCasesByRequestedTime;
   output.impact.hospitalBedsByRequestedTime = mpact4; 
   const mpact5 = (5 / 100) * output.severeImpact.infectionsByRequestedTime;
-
   output.severeImpact.casesForICUByRequestedTime = mpact5;
   const mpact6 = (2 / 100) * output.severeImpact.infectionsByRequestedTime;
   output.severeImpart.casesForVentilatorsByRequestedTime = mpact6;
