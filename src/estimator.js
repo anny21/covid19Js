2134988d2d21feadb276aab277eb523df1b02a4a
unknown <honyinye3@gmail.com>
2020-04-14 18:13:00 +0100

    commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,36 +1,36 @@
+const timeInDays = (time, period) => {
+  if(period === 'days'){
+      return Math.trunc(2 ** ((time * 7) / 3));
+  }else if(period === 'weeks'){
+    return Math.trunc(2 ** ((time * 7) / 3));
+  }else if(period == 'months'){
+    return Math.trunc(2 ** ((time * 30) / 3));
+  }
+}
+
+
+
+
+
+
 const covid19ImpactEstimator = (data) => {
-  const output = { data: {}, impact: {}, severeImpact: {} };
-  output.impact.currentlyInfected = data.reportedCases * 10;
-  output.severeImpact.currentlyInfected = data.reportedCases * 50;
-
-  output.severeImpact.infectionsByRequestedTime = output.severeImpact.currentlyInfected * 512;
-
-  output.impact.infectionsByRequestedTime = output.impact.currentlyInfected * 512;
-
-  const mpact1 = (15 / 100) * (output.severeImpact.infectionsByRequestedTime);
-
-  output.severeImpact.severeCasesByRequestedTime = mpact1;
-
-  const mpact2 = (15 / 100) * (output.impact.infectionsByRequestedTime);
-  output.impact.severeCasesByRequestedTime = mpact2;
-
-  const a = output.severeImpact.severeCasesByRequestedTime;
-  const mpact3 = (35 / 100) * (data.totalHospitalBeds) * a;
-
-  output.severeImpact.hospitalBedsByRequestedTime = mpact3;
-
-  const b = output.impact.severeCasesByRequestedTime;
-
-  const mpact4 = ((35 / 100) * data.totalHospitalBeds) * b;
-  output.impact.hospitalBedsByRequestedTime = mpact4;
-
-  const mpact5 = (5 / 100) * output.severeImpact.infectionsByRequestedTime;
-  output.severeImpact.casesForICUByRequestedTime = mpact5;
-
-  const mpact6 = (2 / 100) * output.severeImpact.infectionsByRequestedTime;
-  output.severeImpart.casesForVentilatorsByRequestedTime = mpact6;
-
-  return output;
+  const estimate = { data: data, impact: {}, severeImpact: {}}
+  const {
+  periodType,
+  timeToElapse,
+  reportedCases,
+  region
+  } = data;
+  const time = timeInDays(timeToElapse, periodType);
+  estimate.impact.currentlyInfected = reportedCases * 10;
+  estimate.severeImpact.currentlyInfected = reportedCases * 50;
+  const x = estimate.impact.currentlyInfected;
+  const y = estimate.severeImpact.currentlyInfected;
+  estimate.impact.infectionsByRequestedTime = x * 2 ** parseInt(time / 3, 10);
+  const z = y * 2 ** parseInt(time / 3, 10);
+  estimate.severeImpact.infectionsByRequestedTime = z;
+  
+  return estimate;
 };
 
 export default covid19ImpactEstimator;
