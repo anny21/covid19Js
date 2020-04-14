23d5a448f08d3171c8672d5be0da3699139f19b8
unknown <honyinye3@gmail.com>
2020-04-14 18:24:39 +0100

    commit

--- src/estimator.js
+++ src/estimator.js
@@ -4,32 +4,27 @@ const timeInDays = (time, period) => {
   } else if (period === 'weeks') {
     Math.trunc(2 ** ((time * 7) / 3));
   } else {
-    period = "months";
     Math.trunc(2 ** ((time * 30) / 3));
   }
-}
-
-
-
-
+};
 
 
 const covid19ImpactEstimator = (data) => {
-  const estimate = { data: data, impact: {}, severeImpact: {}}
+  const estimate = { data: data, impact: {}, severeImpact: {} }
   const {
   periodType,
   timeToElapse,
   reportedCases,
   region
   } = data;
-  const time = timeInDays(timeToElapse, periodType);
-  estimate.impact.currentlyInfected = reportedCases * 10;
-  estimate.severeImpact.currentlyInfected = reportedCases * 50;
-  const x = estimate.impact.currentlyInfected;
-  const y = estimate.severeImpact.currentlyInfected;
-  estimate.impact.infectionsByRequestedTime = x * 2 ** parseInt(time / 3, 10);
-  const z = y * 2 ** parseInt(time / 3, 10);
-  estimate.severeImpact.infectionsByRequestedTime = z;
+    const time = timeInDays(timeToElapse, periodType);
+    estimate.impact.currentlyInfected = reportedCases * 10;
+    estimate.severeImpact.currentlyInfected = reportedCases * 50;
+    const x = estimate.impact.currentlyInfected;
+    const y = estimate.severeImpact.currentlyInfected;
+    estimate.impact.infectionsByRequestedTime = x * 2 ** parseInt(time / 3, 10);
+    const z = y * 2 ** parseInt(time / 3, 10);
+    estimate.severeImpact.infectionsByRequestedTime = z;
 
   return estimate;
 };
